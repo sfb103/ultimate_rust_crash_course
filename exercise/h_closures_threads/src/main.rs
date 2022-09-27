21c5bb0531c072b0cfa614873012e10187de9630
Sean Bentley <sfb103@gmail.com>
2022-09-26 22:49:44 -0700

    solutions

--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -16,8 +16,8 @@ fn expensive_sum(v: Vec<i32>) -> i32 {
     // either need to dereference the argument once in the parameter list like this: `|&x|` or you
     // will need to dereference it each time you use it in the expression like this: `*x`
     v.iter()
-        // .filter() goes here
-        // .map() goes here
+        .filter(|&x| x % 2 == 0)
+        .map(|&x| x.pow(2))
         .sum()
 }
 
@@ -32,7 +32,7 @@ fn main() {
     // join handle in a variable called `handle`. Once you've done this you should be able to run
     // the code and see the Child thread output in the middle of the main thread's letters
     //
-    //let handle = ...
+    let handle = thread::spawn( move|| expensive_sum(my_vector) );
 
     // While the child thread is running, the main thread will also do some work
     for letter in vec!["a", "b", "c", "d", "e", "f"] {
@@ -45,8 +45,8 @@ fn main() {
     // to exit with a `Result<i32, Err>`.  Get the i32 out of the result and store it in a `sum`
     // variable.  Uncomment the println.  If you did 1a and 1b correctly, the sum should be 20.
     //
-    //let sum =
-    //println!("The child thread's expensive sum is {}", sum);
+    let sum = handle.join().unwrap();
+    println!("The child thread's expensive sum is {}", sum);
 
     // Time for some fun with threads and channels!  Though there is a primitive type of channel
     // in the std::sync::mpsc module, I recommend always using channels from the crossbeam crate,
@@ -56,16 +56,15 @@ fn main() {
     // flow of execution works.  Once you understand it, alter the values passed to the `pause_ms()`
     // calls so that both the "Thread B" outputs occur before the "Thread A" outputs.
 
-    /*
-    let (tx, rx) = channel::unbounded();
+    let (tx,rx) = channel::unbounded();
     // Cloning a channel makes another variable connected to that end of the channel so that you can
     // send it to another thread.
     let tx2 = tx.clone();
 
     let handle_a = thread::spawn(move || {
-        pause_ms(0);
+        pause_ms(300);
         tx2.send("Thread A: 1").unwrap();
-        pause_ms(200);
+        pause_ms(400);
         tx2.send("Thread A: 2").unwrap();
     });
 
@@ -74,7 +73,7 @@ fn main() {
     let handle_b = thread::spawn(move || {
         pause_ms(0);
         tx.send("Thread B: 1").unwrap();
-        pause_ms(200);
+        pause_ms(100);
         tx.send("Thread B: 2").unwrap();
     });
 
@@ -89,12 +88,42 @@ fn main() {
     // Join the child threads for good hygiene.
     handle_a.join().unwrap();
     handle_b.join().unwrap();
-    */
+
 
     // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
     // main thread loop through several values and print each out and then send it to the channel.
     // On the child threads print out the values you receive. Close the sending side in the main
     // thread by calling `drop(tx)` (assuming you named your sender channel variable `tx`).  Join
     // the child threads.
+
+    let (tx, rx) = channel::unbounded();
+    let rx2 = rx.clone();
+
+    // let handle1 = thread::spawn( ||receive_msg(1,rx) );
+    // let handle2 = thread::spawn( ||receive_msg(2,rx2) );
+
+    let receive_msg = |id,rx|{
+        for msg in rx {
+            println!("thread {}: Received {}", id, msg);
+            pause_ms(100);
+        }};
+
+    let handle1 = thread::spawn( move||receive_msg(1,rx) );
+    let handle2 = thread::spawn( move||receive_msg(2,rx2) );
+
+    let msgs = vec!["hello", "there", "nice", "to", "meet", "you"];
+    msgs.iter().for_each(|&msg| tx.send(msg).unwrap());
+    drop(tx);
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+
     println!("Main thread: Exiting.")
 }
+
+// fn receive_msg( thread_id: i32, rx: crossbeam::Receiver<&str> ) {
+//     for msg in rx {
+//         println!("thread_id: {}, received msg: {}", thread_id, msg);
+//         pause_ms(100);
+//     }
+// }
